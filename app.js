297fa981dddc4e94306798e9c76d9c019a022751
Anthony Galati <grig.wyrd@gmail.com>
2017-03-12 14:47:39 -0500

    starting Routing Separate Files

--- app.js
+++ app.js
@@ -7,14 +7,63 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views','src/views');
 
 app.set('view engine','ejs');
 
+var books = [
+    {
+        title: 'Book 1',
+        genre: 'Sci Fi',
+        author: 'TGALATI',
+        read: false
+    },
+    {
+        title: 'Book 2',
+        genre: 'Sci Fi',
+        author: 'TGALATI',
+        read: false
+    },
+    {
+        title: 'Book 3',
+        genre: 'Sci Fi',
+        author: 'TGALATI',
+        read: false
+    },
+    {
+        title: 'Book 4',
+        genre: 'Sci Fi',
+        author: 'TGALATI',
+        read: false
+    },
+    {
+        title: 'Book 5',
+        genre: 'Sci Fi',
+        author: 'TGALATI',
+        read: false
+    }
+];
+
+
+
+bookRouter.route('/')
+    .get(function(req,res){
+        res.render('books',{title:'Hello from render',nav:[{Link:'/Books', Text: 'Books'},{Link: '/Authors', Text: 'Authors'}],
+            books:books});
+    });
+
+bookRouter.route('/single')
+    .get(function(req,res){
+        res.send('Hello Single Books');
+    });
+
+app.use('/Books', bookRouter);
+
 app.get('/', function (req, res) {
-    res.render('index',{title:'Hello from render',list:['a','b']});
+    res.render('index',{title:'Hello from render',nav:[{Link:'/Books', Text: 'Books'},{Link: '/Authors', Text: 'Authors'}]});
 });
 app.get('/books', function (req, res) {
     res.send('Hello Books');
